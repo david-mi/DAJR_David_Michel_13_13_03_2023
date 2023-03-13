e8f1345de4d11e097a62c74816e955799922b98a
David Michel <david.michel2809@gmail.com>
2023-03-13 13:24:05 +0100

    feat: add Profile page with default template #9

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
+import Profile from "./Pages/Profile/Profile";
 
 export default () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };

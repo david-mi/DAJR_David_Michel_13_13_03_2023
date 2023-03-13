b66f68c436a78b7a6ca9da77bd9754d01c3354f2
David Michel <david.michel2809@gmail.com>
2023-03-13 13:15:25 +0100

    feat: add Login page #7

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 
 export default () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<Login />} />
     </Routes>
   );
 };

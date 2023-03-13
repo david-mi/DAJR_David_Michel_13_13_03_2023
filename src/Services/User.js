aecf685b7fd220d041b674b9e8ae74ded7045fed
David Michel <david.michel2809@gmail.com>
2023-03-13 13:56:21 +0100

    feat: start to implement User service, with login method #10

---
+++ src/Services/User.js
@@ -0,0 +1,30 @@
+class User {
+  #API_USER_URL = `${process.env.REACT_APP_API_BASE_URL}/user`;
+
+  async #fetchOrThrow(endpoint, options) {
+    const url = `${this.#API_USER_URL}/${endpoint}`;
+    const response = await fetch(url, options);
+    if (response.ok === false) {
+      throw new Error(response.statusText);
+    }
+
+    return await response.json();
+  }
+
+  async login(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    };
+
+    const { body } = await this.#fetchOrThrow("/login", options);
+    return body;
+  }
+}
+
+const user = new User();
+
+export default user;

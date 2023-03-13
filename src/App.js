deb6afc25b39ed3529b54afd64b9230d1771c166
David Michel <david.michel2809@gmail.com>
2023-03-13 11:21:41 +0100

    feat: add Home page #1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './styles/main.css';
+import Home from "./Pages/Home/Home";
 
 const App = () => {
   return (
     <div className="App">
-      console
+      <Home />
     </div>
   );
 };

be51fca148d22ec239342c8800b91bfc20a594c1
David Michel <david.michel2809@gmail.com>
2023-03-13 11:43:22 +0100

    feat: refacto Home into Features, Hero component #4

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,2 +1,4 @@
 export { default as Nav } from "./Nav/Nav";
+export { default as Hero } from "./Hero/Hero";
+export { default as Features } from "./Features/Features";
 export { default as Footer } from "./Footer/Footer";

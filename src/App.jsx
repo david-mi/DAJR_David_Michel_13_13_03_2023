166c3683a3f33440fbad6e506bcca541c2a0927f
David Michel <david.michel2809@gmail.com>
2023-03-14 16:34:07 +0100

    feat: add font awesome cdn and change page title #25

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import { Provider } from "react-redux";
 import Routes from "./Routes/Routes";
 import { Footer, Nav } from "./Components";
 import store from "./store";
+const navProps = {
+  image: {
+    src: "./argentBankLogo.png",
+    alt: "Argent Bank Logo"
+  },
+  title: "Argent Bank"
+};
 
 const App = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Nav />
+        <Nav {...navProps} />
         <Routes />
         <Footer />
       </BrowserRouter>

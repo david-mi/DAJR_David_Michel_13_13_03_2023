0b36b7fceb05bd6d431eaa4f1a99a678b7525495
David Michel <david.michel2809@gmail.com>
2023-03-18 11:32:23 +0100

    feat: add Loader component #40

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,21 @@
+import styles from "./loader.module.css";
+const FormLoader = ({ className }) => {
+  return (
+    <div className={`${styles.loader} ${className}`}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default FormLoader;

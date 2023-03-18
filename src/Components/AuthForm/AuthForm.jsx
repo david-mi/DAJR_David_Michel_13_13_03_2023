4537b0b311b6d3d4bddcebfc0c26487a6488cfb3
David Michel <david.michel2809@gmail.com>
2023-03-18 11:33:46 +0100

    feat: display loader and disabling form elements when sending login request #40

--- src/Components/AuthForm/AuthForm.jsx
+++ src/Components/AuthForm/AuthForm.jsx
@@ -1,11 +1,15 @@
 import { useRef } from "react";
 import styles from "./authForm.module.css";
+import Loader from "../../Components/Loader/Loader";
+import { useSelector } from "react-redux";
+import { fetchStatus } from "../../reducers/enums";
 
 const AuthForm = ({ submitHandler }) => {
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
   const formRef = useRef(null);
   const { status, error } = useSelector(({ profile }) => profile.login);
+  const isSendingRequest = status === fetchStatus.PENDING;
 
   function submitForm(event) {
     event.preventDefault();
@@ -33,6 +37,7 @@ const AuthForm = ({ submitHandler }) => {
           id="email"
           ref={emailInputRef}
           required
+          disabled={isSendingRequest}
         />
       </div>
       <div className="input-wrapper">
@@ -44,13 +49,17 @@ const AuthForm = ({ submitHandler }) => {
           pattern="(?=.*[a-z])(?=.*\d)[\w]{5,}"
           title="Le mot de passe doit contenir au minimum 6 lettres dont une minuscule et un chiffre"
           required
+          disabled={isSendingRequest}
         />
       </div>
       <div className="input-remember">
-        <input type="checkbox" id="remember-me" />
+        <input type="checkbox" id="remember-me" disabled={isSendingRequest} />
         <label htmlFor="remember-me">Remember me</label>
       </div>
-      <button className="sign-in-button">Sign In</button>
+      <button className="sign-in-button" disabled={isSendingRequest}>
+        Sign In
+        {isSendingRequest && <Loader className={styles.loader} />}
+      </button>
       {error && <small className={styles.error}>{error}</small>}
     </form>
   );
